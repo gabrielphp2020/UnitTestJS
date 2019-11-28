1fcb5e5ac975b71993512676edbf4a619d77fc76
gabriel <tetradog@gmail.com>
2019-11-28 08:54:04 +0100

    desarrollando :)

--- Assert.js
+++ Assert.js
@@ -1,140 +1,138 @@
-window.Import(window._ROOTUTILS+"ArrayUtils.js");
-
-window.Import(window._ROOTUTILS+"ReflectionUtils.js");
-
-
-class Assert{
-
-static get Methods(){
-
-if(!Assert._methods||Assert._methods===null)
-Assert._methods=[];
-
-return Assert._methods;
+window.Import(window._ROOTUTILS + "ArrayUtils.js");
+window.Import(window._ROOTUTILS + "ReflectionUtils.js");
 
 
-}
+class Assert {
 
-static set Methods(methods){
+    static get Methods() {
 
+        if (!Assert._methods || Assert._methods === null)
+            Assert._methods = [];
 
-if(methods!== null)
-{
-
-/*Miro que haya [[campos]]*/
-
-Assert._methods=methods;
-
-}else Assert._methods=null;
+        return Assert._methods;
 
 
-}
-static get OnObsolete(){
-if(!Assert._obsolete)
-{
-Assert._obsolete=(clase,func)=>{
-console.error("function to test not found\""+clase+"."+func+"\"");
-};
-}
-return Assert._obsolete;
+    }
 
-}
-static set OnObsolete (onObsolete){
-
-Assert._obsolete=onObsolete;
-
-}
-
-static get OnError(){
-if(!Assert._error)
-{
-Assert._error=(clase,func,error,posTest)=>{
-console.error("function to test error \""+clase+"."+func+"\" posTest="+posTest);
-console.error(error);
-};
-}
-return Assert._error;
-
-}
-static set OnError (onError){
-
-Assert._error=onError;
-
-}
-
-static get OnSuccess(){
-if(!Assert._success)
-{
-Assert._success=(clase,func,posTest)=>{
-console.log("function to test success \""+clase+"."+func+"\" posTest="+posTest);
-};
-}
-return Assert._success;
-
-}
-static set OnSuccess (onSuccess){
-
-Assert._success=onSuccess;
-
-}
+    static set Methods(methods) {
 
 
-static Init(){
-return new Promise((okey,error)=>{
+        if (methods !== null) {
 
-const CLASS=0;
-const METHOD=1;
-const METHODTOTEST=2;
+            /*Miro que haya [[campos]]*/
 
-var testingMethods=[];
-var testActual;
+            Assert._methods = methods;
 
-for(var i=0; i<Assert.Methods.length;i++)
-{
-testActual=Assert.Methods[i];
-
-/*Class,MethodToTest,TesterMethod*/
-if(ReflectionUtils.ExistFunction(testActual[CLASS], testActual[METHOD])){
-ArrayUtils.Add(testingMethods, Assert._ExecuteMethod(testActual[CLASS],testActual[METHOD], testActual [TESTMETHOD],i));
-
-}else{
-/*No existe el metodo a testear*/
-Assert.OnObsolete(testActual[CLASS], testActual[METHOD]);
-}
+        } else Assert._methods = null;
 
 
-}
+    }
+    static get OnObsolete() {
+        if (!Assert._obsolete) {
+            Assert._obsolete = (clase, func) => {
+                console.error("function to test not found\"" + clase + "." + func + "\"");
+            };
+        }
+        return Assert._obsolete;
 
-Promise.all(testingMethods).then(()=>{okey();});
+    }
+    static set OnObsolete(onObsolete) {
+
+        Assert._obsolete = onObsolete;
+
+    }
+
+    static get OnError() {
+        if (!Assert._error) {
+            Assert._error = (clase, func, error, posTest) => {
+                console.error("function to test error \"" + clase + "." + func + "\" posTest=" + posTest);
+                console.error(error);
+            };
+        }
+        return Assert._error;
+
+    }
+    static set OnError(onError) {
+
+        Assert._error = onError;
+
+    }
+
+    static get OnSuccess() {
+        if (!Assert._success) {
+            Assert._success = (clase, func, posTest) => {
+                console.log("function to test success \"" + clase + "." + func + "\" posTest=" + posTest);
+            };
+        }
+        return Assert._success;
+
+    }
+    static set OnSuccess(onSuccess) {
+
+        Assert._success = onSuccess;
+
+    }
+
+
+    static Init() {
+        return new Promise((okey, error) => {
+
+            const CLASS = 0;
+            const METHOD = 1;
+            const TESTMETHOD = 2;
+
+            var testingMethods = [];
+            var testActual;
+
+            for (var i = 0; i < Assert.Methods.length; i++) {
+                testActual = Assert.Methods[i];
+
+                /*Class,MethodToTest,TesterMethod*/
+                if (ReflectionUtils.ExistFunction(testActual[CLASS], testActual[METHOD])) {
+                    ArrayUtils.Add(testingMethods, Assert._ExecuteMethod(testActual[CLASS], testActual[METHOD], testActual[TESTMETHOD], i));
+
+                } else {
+                    /*No existe el metodo a testear*/
+                    Assert.OnObsolete(testActual[CLASS], testActual[METHOD]);
+                }
+
+
+            }
+
+            Promise.all(testingMethods).then(() => { okey(); });
 
 
 
-});
+        });
 
 
 
-}
+    }
 
 
-static _ExecuteMethod(clase,func, testMethod,position){
+    static _ExecuteMethod(clase, func, testMethod, position) {
 
-return new Promise((okey,error)=>{
-var aux=testMethod ();
-if( aux instanceof Promise)
-aux.then(okey);
-else okey();
+        return new Promise((okey, error) => {
+            try {
+                var aux = testMethod();
+                if (aux instanceof Promise)
+                    aux.then(okey).catch(error);
+                else okey();
+            } catch (ex) { error(ex); }
 
-}).then(()=>{
-Assert.OnSuccess(clase,func, position);
+        }).then(() => {
+            Assert.OnSuccess(clase, func, position);
 
-}).catch((error)=>{
-/*Trato error de ejecución*/
-Assert.OnError(clase, func,error,position);
-});
-
+        }).catch((error) => {
+            /*Trato error de ejecución*/
+            Assert.OnError(clase, func, error, position);
 
 
-}
+        });
+
+
+
+    }
 
 
 
